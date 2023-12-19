cab2bd905cdc1bcb4c1df3af70a8ab03bf45a8dd
e1980 <bryan@codeapi.cc>
2023-12-19 15:29:59 +0800

    init on Omen

---
+++ src/main/java/ApplicationStarter.java
@@ -0,0 +1,8 @@
+import com.bffapi.onsumingrest.ConsumingRestApplication;
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class, args);
+    }
+}
